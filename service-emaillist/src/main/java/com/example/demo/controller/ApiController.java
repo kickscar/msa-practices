761b8acd1792d6cda4a517b3d328b8e44494e298
kickscar <kickscar@gmail.com>
2022-04-03 08:49:26 +0900

    first commit

---
+++ service-emaillist/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.EmaillistRepository;
+import com.example.demo.vo.EmaillistVo;
+
+@RestController
+// @CrossOrigin(origins = {"http://localhost:9999"}, allowedHeaders = "*", allowCredentials="false", methods={RequestMethod.GET})
+public class ApiController {
+	
+	@Autowired
+	private EmaillistRepository emaillistRepository;
+	
+	@GetMapping("/api")
+	public ResponseEntity<Object> read(@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(emaillistRepository.findAll(keyword));
+	}
+	
+	@PostMapping("/api")
+	public ResponseEntity<Object> create(@RequestBody EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(vo);
+	}
+}
