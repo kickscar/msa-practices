7127cb088b4c43e4c0f2772bdbc74dc946143ec5
kickscar <kickscar@gmail.com>
2022-04-06 21:35:29 +0900

    deploy #2

--- servers/service-gateway-server/src/main/resources/script/launch.sh
+++ servers/service-gateway-server/src/main/resources/script/launch.sh
@@ -4,13 +4,13 @@ SERVER_NAME=service-gateway-server
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 PID=$(ps -ef | grep java | grep $SERVER_NAME.jar | awk '{print $2}')
 
-if  [ -n "$PID" ]
+if  [ ! -z "$PID" ]
 then
 	echo "stopping [$SERVER_NAME]"
-	kill -9 $"PID"
+	kill -9 $PID
 	sleep 10
 fi
 
 echo "starting [$SERVER_NAME]"
-cd $"SCRIPT_DIR" || exit
-nohup java -Dspring.profiles.active=production -jar $"SCRIPT_DIR"/$SERVER_NAME.jar >> $"SCRIPT_DIR"/launch.log &
+cd $SCRIPT_DIR || exit
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$SERVER_NAME.jar >> $SCRIPT_DIR/launch.log &
